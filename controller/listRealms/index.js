55643030ce7de3efad36458b213e3395eac9bbfb
christophrus <christophrus@protonmail.com>
2019-10-11 16:59:39 +0200

    better realm sorting

--- controller/listRealms/index.js
+++ controller/listRealms/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-underscore-dangle */
 const Time = require('../../models/Time');
 
 module.exports = cb => {
   Time.aggregate([{ $group: { _id: '$realm', sum: { $sum: 1 } } }], (error, data) => {
     if (error) return cb({ status: 500, message: 'Database error', trace: error });
     if (data) {
-      // eslint-disable-next-line no-underscore-dangle
-      const realms = data.map(element => element._id).sort((a, b) => a.localeCompare(b));
+      const realms = data
+        .map(element => element._id)
+        .sort((a, b) => a.substring(5).localeCompare(b.substring(5)));
       return cb(null, { realms });
     }
     return cb({ status: 500, message: 'No realms found' });
