43a143f0b4a0a966b839fa95cae29e23c3b830b9
christophrus <christophrus@protonmail.com>
2019-11-14 13:09:02 +0100

    set date hours/minutes/seconds for last seeen to 0

--- controller/getQuickStats/index.js
+++ controller/getQuickStats/index.js
@@ -20,6 +20,10 @@ module.exports = (req, cb) => {
 
   if (query.hasOwnProperty('lastSeen')) {
     const date = new Date();
+    date.setHours(0);
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
     date.setDate(date.getDate() - Number(query.lastSeen));
     query.lastSeen = { $gte: date };
   }
